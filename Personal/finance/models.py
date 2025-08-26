d17debe9e79127c68d2f5aedf9566d89ba14c0fb
Ciriossss <domenicociriello2000@gmail.com>
2025-08-26 23:59:46 +0200

    creation of finence models

--- Personal/finance/models.py
+++ Personal/finance/models.py
@@ -1,3 +1,84 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from user.models import Profile
+
+
+class AssetClass(models.Model):
+    id = models.AutoField(primary_key=True)
+    account_class = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.account_class
+
+class Instrument(models.Model):
+    id = models.AutoField(primary_key=True)
+    instrument_code = models.CharField(max_length=10)
+    instrument_name = models.CharField(max_length=255)
+    account_class = models.ForeignKey(AssetClass, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.instrument_code
+    
+
+class Account(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=255)
+    account_class = models.ForeignKey(AssetClass, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.id
+    
+class Investment(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now) 
+    instrument_code = models.ForeignKey(Instrument,on_delete = models.CASCADE)
+    quantity = models.DecimalField(max_digits=15, decimal_places=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.id
+
+class TransactionCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    category = models.CharField(max_length=255)
+    def __str__(self):
+        return self.id
+    
+class Label(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    label = models.CharField(max_length=255)
+    def __str__(self):
+
+        return self.label
+       
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category_id = models.ForeignKey(TransactionCategory,on_delete=models.CASCADE)
+    label_id = models.ForeignKey(Label,on_delete = models.CASCADE, blank=True, null=True)
+    account_id = models.ForeignKey(Account,on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Transaction-{self.id}"
+
+class Wallet(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    account_id = models.ForeignKey(Account,on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.author.id}-{self.date.strftime('%Y-%m')}-{self.account}"
