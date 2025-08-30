9f39e3bd07aafe1c8afd4dadb750567e52f381c9
Ciriossss <domenicociriello2000@gmail.com>
2025-08-31 00:19:22 +0200

    first chart.js

--- Personal/dashboard/views.py
+++ Personal/dashboard/views.py
@@ -4,6 +4,7 @@ from django.db.models import Sum,F
 from Personal.utils import *
 from finance.models import *
 import datetime
+import calendar
 
 @login_required
 def dashboard(request):
@@ -18,12 +19,19 @@ def dashboard(request):
         prior_month = current_month - 1
         prior_year = current_year
 
-    # CARD 1 : Net Activity (Transactions) this month
+    current_month_name = calendar.month_name[current_month]
+
+
+    ##################################
+    ## Finance Dashboard card data  ##
+    ##################################
+
+    #CARD 1 : Net Activity (Transactions) this month
     net_activity = Transaction.objects.filter(author=request.user, date__year=current_year, date__month=current_month).aggregate(net_activity = Sum('amount'))['net_activity'] or 0
     budget = 500
     money_to_budget = net_activity + budget
 
-    # CARD 2 : Change in Wealth (Wallet) this month vs last month
+    #CARD 2 : Change in Wealth (Wallet) this month vs last month
     prior_wealth = Wallet.objects.filter(author=request.user,date__year=prior_year, date__month=prior_month).aggregate(prior_wealth = Sum('amount'))['prior_wealth'] or 0
     current_wealth = (Wallet.objects.filter(author=request.user,date__year=current_year, date__month=current_month).aggregate(current_wealth = Sum('amount'))['current_wealth'] or 0) + net_activity
     change_prc = round(((current_wealth - prior_wealth) / prior_wealth * 100) if prior_wealth != 0 else 0,0)
@@ -36,10 +44,22 @@ def dashboard(request):
     investment_profit_prc = round(investment_profit / investment_cost * 100,2) if investment_cost !=0 else 0
 
     #CARD 4 : TOP % OF POPULATION
-
     population_perc = percentage_population_function(current_wealth)
 
+    ##################################
+    ##     Finance data             ##
+    ##################################
+
+    #WEALTH EVOLUTION
+    wallet_data = Wallet.objects.filter(author=request.user)
+
+
+
+
+
     context = {
+        'current_month_name':current_month_name,
+        'current_year':current_year,
         'net_activity': net_activity, 
         'current_wealth': current_wealth,
         'money_to_budget':money_to_budget,
@@ -47,7 +67,8 @@ def dashboard(request):
         'investment_cost': investment_cost,
         'investment_profit':investment_profit,
         'investment_profit_prc':investment_profit_prc,
-        'population_perc':population_perc
+        'population_perc':population_perc,
+        'wallet_data':wallet_data
     }
 
     return render (request,'dashboard/dashboard.html',context)
