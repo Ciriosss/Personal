53f65b69698597603fe728df0406a0fc4510ac65
Ciriossss <domenicociriello2000@gmail.com>
2025-09-14 23:41:48 +0200

    added fitness models

--- Personal/fitness/models.py
+++ Personal/fitness/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.utils import timezone
+from user.models import Profile
 
-# Create your models here.
+class Intensity(models.Model):
+    id = models.AutoField(primary_key=True)
+    intensity = models.CharField(max_length=10)
+    description = models.CharField(max_length=30)
+    def __str__(self):
+        return self.intensity
+
+class Type(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.type
+
+class Exercise(models.Model):
+    id = models.AutoField(primary_key=True)
+    exercise = models.CharField(max_length=30)
+    type = models.ForeignKey(Type,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.exercise
+    
+class PhisicalActivity (models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    time = models.IntegerField()
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    intensity = models.ForeignKey(Intensity,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Activity({self.id})"
+    
+class Diet (models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    need_cal = models.IntegerField()
+    eaten_cal = models.IntegerField()
+
+    def __str__(self):
+        return f"Diet({self.id})"
+    
+class Cali(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    series = models.IntegerField()
+    rep = models.IntegerField()
+    extra = models.IntegerField()
+
+    def __str__(self):
+        return f"Cali({self.id})"
+    
+class Cardio(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    distance = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.IntegerField(null= True, blank =True)
+
+    def __str__(self):
+        return f"Cardio({self.id})"
+    
+class Body(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    waist = models.DecimalField(max_digits=10, decimal_places=2)
+    bicept = models.DecimalField(max_digits=10, decimal_places=2)
+    leg = models.DecimalField(max_digits=10, decimal_places=2)
+    BMI = models.DecimalField(max_digits=10, decimal_places=2)
+    muscle_mass = models.DecimalField(max_digits=10, decimal_places=2,default = 0)
+    body_water = models.DecimalField(max_digits=10, decimal_places=2,default = 0)
+
+    def __str__(self):
+        return f"Body({self.id})"
+    
+
+class Max(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    entry_date = models.DateTimeField(default=timezone.now)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    max = models.DecimalField(max_digits=10, decimal_places=2)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Cardio({self.id})"
