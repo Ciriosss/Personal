8807d173513a43be720c95bbcf1a5670091a4be5
Ciriossss <domenicociriello2000@gmail.com>
2025-10-02 00:12:24 +0200

    transactions part 1

--- Personal/finance/views.py
+++ Personal/finance/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+import datetime
+import calendar
 from .models import *
 
 def transactions(request):
-    transactions = Transaction.objects.filter(author=request.user).order_by('-date')
+
+    current_year = datetime.datetime.now().year
+    current_month = datetime.datetime.now().month
+
+    user = request.user
+
+    if current_month == 1:
+        prior_month = 12
+        prior_year = current_year - 1
+    else:
+        prior_month = current_month - 1
+        prior_year = current_year
+
+    current_month_name = calendar.month_name[current_month]
+    transactions = Transaction.objects.filter(author=request.user,date__year=current_year, 
+                    date__month=current_month).order_by('-date')
 
     context = {
-        'transactions': transactions
+        'transactions': transactions,
+        'current_month_name':current_month_name,
+        'current_year':current_year,
     }
     return render(request, 'finance/transaction.html',context)
 
