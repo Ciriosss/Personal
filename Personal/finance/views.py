8f395d1af16036676054f95013ab18df26c42965
Ciriossss <domenicociriello2000@gmail.com>
2025-10-02 00:35:37 +0200

    transactions 2

--- Personal/finance/views.py
+++ Personal/finance/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum,F,Count,Case,When
+from django.db.models.functions import Abs
 import datetime
 import calendar
 from .models import *
@@ -9,24 +11,20 @@ def transactions(request):
     current_year = datetime.datetime.now().year
     current_month = datetime.datetime.now().month
 
-    user = request.user
-
-    if current_month == 1:
-        prior_month = 12
-        prior_year = current_year - 1
-    else:
-        prior_month = current_month - 1
-        prior_year = current_year
-
     current_month_name = calendar.month_name[current_month]
-    transactions = Transaction.objects.filter(author=request.user,date__year=current_year, 
-                    date__month=current_month).order_by('-date')
+    transactions = Transaction.objects.filter(author=request.user,date__year=current_year,date__month=current_month).order_by('-date')
+
+    categories_recap = Transaction.objects.filter(author=request.user,date__year=current_year, date__month=current_month).exclude(type='Transfer').values('category_id__category').annotate(total_amount=Sum(Case(When(type='Expense', then=F('amount') * -1),
+                                default=F('amount')
+                    ))).annotate(count=Count('amount')).order_by(Abs('total_amount').desc())[:5]
 
     context = {
         'transactions': transactions,
         'current_month_name':current_month_name,
         'current_year':current_year,
+        'categories_recap':categories_recap,
     }
+        
     return render(request, 'finance/transaction.html',context)
 
 def accounts(request):
