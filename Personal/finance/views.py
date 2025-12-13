59ff660a763101af7728611937774014324ccb95
Ciriossss <domenicociriello2000@gmail.com>
2025-12-13 17:30:47 +0100

    investment summary

--- Personal/finance/views.py
+++ Personal/finance/views.py
@@ -70,7 +70,151 @@ def investments(request):
     investments = Investment.objects.filter(author=request.user)
     investments_count = investments.count()
 
-    query = """ 
+    investment_summary_query = """
+                WITH investments AS (
+                                
+                            SELECT 
+                                id,
+                                author_id,
+                                instrument_code_id,
+                                date,
+                                price,
+                                quantity,
+                                price * quantity AS invested,
+                                SUM(quantity) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date) AS cum_quantity,
+                                SUM(price * quantity) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date) AS cum_invested,
+                                IIF(
+                                    quantity < 0,
+                                    ROUND(
+                                        (price * SUM(quantity) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date))
+                                        - SUM(price * quantity) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date)
+                                    ,2)
+                                ,0) AS adjustment,
+                                IIF(quantity >= 0, 'Buy', 'Sell') AS operation
+                            FROM 
+                                finance_investment
+                            WHERE 
+                                author_id = %s
+                            ORDER BY 
+                                author_id,
+                                instrument_code_id,
+                                date
+            ),
+                            drill_down_adjustment AS (
+                                SELECT
+                                    id, 
+                                    author_id,
+                                    instrument_code_id,
+                                    date,
+                                    price,
+                                    quantity,
+                                    invested,
+                                    cum_quantity,
+                                    cum_invested,
+                                    IIF(
+                                adjustment = 0 
+                                AND LAG(adjustment) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date) <> 0,
+                                LAG(adjustment) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date),
+                                adjustment
+                            ) AS adjustment,
+                                    operation
+                                FROM
+                                    investments
+                            ),
+                            investments_adj AS(
+                                SELECT
+                                    id, 
+                                    author_id,
+                                    instrument_code_id,
+                                    date,
+                                    price,
+                                    quantity,
+                                    invested,
+                                    cum_quantity,
+                                    cum_invested  
+                                    + COALESCE(LAG(adjustment) OVER (PARTITION BY author_id, instrument_code_id ORDER BY date),0)
+                                    AS cum_invested,
+                                    adjustment,
+                                    operation
+                                FROM
+                                    drill_down_adjustment
+                            ),
+                            investments_recalculation AS (
+                                SELECT
+                                    id, 
+                                    author_id,
+                                    instrument_code_id,
+                                    date,
+                                    price,
+                                    quantity,
+                                    invested,
+                                    cum_quantity,
+                                    cum_invested ,
+                                    operation,
+                                    IIF(operation = 'Buy',ROUND((price * cum_quantity) - cum_invested,2),0)  as urgl,
+                                    IIF(operation = 'Sell',ROUND((price * cum_quantity) - cum_invested,2),0) as rgl
+                                FROM investments_adj
+                            ),
+                            datamart AS (
+                        SELECT 
+                                Investment.*,
+                                Instrument.instrument_code AS instrument_code,
+                                Instrument.instrument_name AS instrument_name
+                        FROM investments_recalculation Investment
+                        LEFT JOIN finance_instrument instrument
+                        ON 1 = 1
+                            AND Investment.instrument_code_id = instrument.id),
+                        RGL AS (
+                            SELECT 
+                                instrument_code_id,
+                                instrument_code,
+                                instrument_name,
+                                SUM(RGL) AS RGL,
+                                COUNT(RGL) AS investment_count
+                            FROM datamart
+                            GROUP BY 
+                                instrument_code_id,
+                                instrument_code,
+                                instrument_name
+                            
+                        ),
+                        URGL AS (
+                            SELECT 
+                                dt.instrument_code_id,
+                                dt.instrument_code,
+                                dt.instrument_name,
+                                dt.URGL
+                                
+                            FROM datamart dt
+                            JOIN (
+                                SELECT 
+                                    instrument_code_id,
+                                    instrument_code,
+                                    instrument_name,
+                                    MAX(Date) AS last_date
+                                FROM datamart
+                                GROUP BY 
+                                    instrument_code_id,
+                                    instrument_code,
+                                    instrument_name) AS last_date
+                            ON 1 = 1
+                                AND dt.instrument_code_id = last_date.instrument_code_id
+                                AND dt.date = last_date.last_date
+                        )
+            SELECT 
+                RGL.instrument_code_id,
+                RGL.instrument_code,
+                RGL.instrument_name,
+                investment_count,
+                URGL,
+                RGL
+
+            FROM RGL
+            FULL OUTER JOIN URGL
+            ON RGL.instrument_code_id = URGL.instrument_code_id
+""" % (request.user.id)
+
+    investment_evolution_query = """ 
             WITH investments AS (
                     
                 SELECT 
@@ -165,19 +309,33 @@ def investments(request):
 				AND Investment.instrument_code_id = instrument_code_id""" % (request.user.id)
 
     with connections['default'].cursor() as cursor:
-        cursor.execute(query)
+        cursor.execute(investment_evolution_query)
         columns = [col[0] for col in cursor.description]  # Get column names
         rows = cursor.fetchall()
 
-    investment_recap = []
+    investment_evolution = []
     for row in rows:
-        investment_recap.append(dict(zip(columns, row)))
+        investment_evolution.append(dict(zip(columns, row)))
 
-    print(type(investment_recap))
+    with connections['default'].cursor() as cursor:
+        cursor.execute(investment_summary_query)
+        columns = [col[0] for col in cursor.description]  # Get column names
+        rows = cursor.fetchall()
+
+    investment_summary = []
+    for row in rows:
+        investment_summary.append(dict(zip(columns, row)))
+
+    total_rgl = round(sum(item['RGL'] for item in investment_summary if item['RGL'] is not None),2)
+    total_urgl = round(sum(item['URGL'] for item in investment_summary if item['URGL'] is not None),2)
+
     contex = {
         'investments_list': investments_list,
         'investments_count': investments_count,
-        'investment_recap': investment_recap
+        'investment_evolution': investment_evolution,
+        'investment_summary': investment_summary,
+        'total_rgl': total_rgl,
+        'total_urgl': total_urgl
     }
 
     return render(request, 'finance/investment.html',contex)
